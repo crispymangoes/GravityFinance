62b5f0c385bc542c0b7dda7741197627b0879d9e
crispymangoes <crispymangoes@gmail.com>
2021-05-03 13:38:58 -0700

    Create IDO tests

---
+++ hardhat.config.js
@@ -0,0 +1,52 @@
+require("@nomiclabs/hardhat-waffle");
+const { mnemonic } = require('./secrets.json');
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async () => {
+  const accounts = await ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+module.exports = {
+  solidity: {
+    compilers: [
+      {
+        version: "0.6.6",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 1000
+          }
+        }
+      },
+      {
+        version: "0.8.4",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 1000
+          }
+        }
+      }
+    ]
+  },
+  networks: {
+    mumbai: {
+      url: `https://rpc-mumbai.maticvigil.com`,
+      accounts: {mnemonic: mnemonic}
+    },
+    polygon: {
+      url: `https://rpc-mainnet.maticvigil.com`,
+      accounts: {mnemonic: mnemonic}
+    },
+    xDai: {
+      url: `https://rpc.xdaichain.com/`,
+      accounts: {mnemonic: mnemonic}
+    }
+  },
+  mocha: {
+    timeout: 200000
+  }
+};
