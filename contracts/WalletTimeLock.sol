9dd1a1c88e65779987c865873cbcb8104519438f
CrispyMangoes <crispymangoes@gmail.com>
2021-06-28 12:32:06 -0700

    Add in logic so that multiple wallets can have assets withdrawn to them

--- contracts/WalletTimeLock.sol
+++ contracts/WalletTimeLock.sol
@@ -9,10 +9,11 @@ contract WalletTimeLock is Ownable {
 
     uint public lockLength; //Amount of time that needs to pass in order for a request to become valid
     uint public graceLength; //Amount of time owner has to call the function before it expires
-    
-    uint public withdrawERC20_timestamp;
-    address public withdrawERC20_token;
-    address public withdrawERC20_wallet;
+
+    mapping(address => uint) public withdrawERC20_timestamp;
+    mapping(address => address) public withdrawERC20_token;
+    mapping(address => uint) public withdrawERC20_amount;
+    //mapping(address => address) public withdrawERC20_wallet;
     
     /**
     * @dev emitted when proposal to withdraw ERC20 tokens from this contract is made
@@ -32,21 +33,22 @@ contract WalletTimeLock is Ownable {
     * @param token the address of the ERC20 token you want to withdraw from this contract
     * @param wallet the address of the reciever of withdrawn tokens
     **/
-    function withdrawERC20(address token, address wallet) external onlyOwner{
-        uint validStart = withdrawERC20_timestamp + lockLength;
-        uint validEnd = withdrawERC20_timestamp + lockLength + graceLength;
+    function withdrawERC20(address token, address wallet, uint amount) external onlyOwner{
+        uint validStart = withdrawERC20_timestamp[wallet] + lockLength;
+        uint validEnd = withdrawERC20_timestamp[wallet] + lockLength + graceLength;
 
         if (block.timestamp > validStart && block.timestamp < validEnd){//If request is now valid fulfill it
-            withdrawERC20_timestamp = 0; //reset the timestamp
-            IERC20 Token = IERC20(withdrawERC20_token);
-            Token.transfer(withdrawERC20_wallet, Token.balanceOf(address(this)));
+            withdrawERC20_timestamp[wallet] = 0; //reset the timestamp
+            IERC20 Token = IERC20(withdrawERC20_token[wallet]);
+            Token.transfer(wallet, withdrawERC20_amount[wallet]);
         }
 
         else{ //Call is not valid so reset timestamp and capture input
             if(block.timestamp > validEnd){//Only make a new timestamp if the current one is expired
-                withdrawERC20_timestamp = block.timestamp;
-                withdrawERC20_wallet = wallet;
-                withdrawERC20_token = token;
+                withdrawERC20_timestamp[wallet] = block.timestamp;
+                //withdrawERC20_wallet = wallet;
+                withdrawERC20_amount[wallet] = amount;
+                withdrawERC20_token[wallet] = token;
                 emit withdraw(validStart, token, wallet);//emit for the world to see
             }
         }
